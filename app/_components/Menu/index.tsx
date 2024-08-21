172f7d12b233bef8f80d542ab416c9b93477c3ce
KeitaHagiwara <73115065+KeitaHagiwara@users.noreply.github.com>
2024-08-21 17:07:00 +0900

    ハンバーガーメニューのクリックで閉じるようにする

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -9,34 +9,35 @@ import styles from './index.module.css';
 export default function Menu() {
 
   const [isOpen, setOpen] = useState<boolean>(false);
-  const open = () => setOpen(true);
-  const close = () => setOpen(false);
+  const toggleNav = () => {
+    setOpen(!isOpen);
+  };
 
   return (
     <div>
-      <nav className={cx(styles.nav, isOpen && styles.open)}>
+      <nav className={cx(styles.nav, isOpen ? styles.open: styles.close)}>
         <ul className={styles.items}>
           <li>
-            <Link href="/">トップ</Link>
+            <Link href="/" onClick={toggleNav}>トップ</Link>
           </li>
           <li>
-            <Link href="/business">事業内容</Link>
+            <Link href="/business" onClick={toggleNav}>事業内容</Link>
           </li>
           <li>
-            <Link href="/portfolio">ポートフォリオ</Link>
+            <Link href="/portfolio" onClick={toggleNav}>ポートフォリオ</Link>
           </li>
           <li>
-            <Link href="/news">ニュース</Link>
+            <Link href="/news" onClick={toggleNav}>ニュース</Link>
           </li>
           <li>
-            <Link href="/contact">お問い合わせ</Link>
+            <Link href="/contact" onClick={toggleNav}>お問い合わせ</Link>
           </li>
         </ul>
-        <button className={cx(styles.button, styles.close)} onClick={close}>
+        <button className={cx(styles.button, styles.close)} onClick={toggleNav}>
           <Image src="close.svg" alt="閉じる" width="24" height="24" />
         </button>
       </nav>
-      <button className={styles.button} onClick={open}>
+      <button className={styles.button} onClick={toggleNav}>
         <Image src="menu.svg" alt="メニュー" width="24" height="24" />
       </button>
     </div>
